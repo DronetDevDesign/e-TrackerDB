db6bb5ac279b2b21d8706547f2500e5f4c93e77a
Ron Dronet <ron@dronetdevdesign.com>
2022-08-12 12:05:38 -0500

    created the tables in schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,28 @@
--- DEPARTMENT: 
--- id: INT PRIMARY KEY
--- name: VARCHAR(30) - holds departments name
+DROP TABLE IF EXISTS departments;
 
--- ROLE:
--- id: INT PRIMARY KEY
--- title: VARCHAR(30) - to hold role title
--- salary: DECIMAL - to hold salary 
--- department_id: INT - to hold reference to department role belongs to 
+DROP TABLE IF EXISTS roles;
 
--- EMPLOYEE: 
--- id: INT PRIMARY KEY
--- first_name: VARCHAR(30) - to hold the employee first name
--- last_name: VARCHAR(30) - to hold the employee last name
--- role_id: INT - to hold reference to employee role
--- manager_id: INT - to hold reference to another employee that is the manager of the current employee (
-                  -- null if the employee has no manager)
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE
+  departments (
+    id INT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+  );
+
+CREATE TABLE
+  roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT
+  );
+
+CREATE TABLE
+  employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+  );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,24 @@
-INSERT INTO
-  department (name)
-VALUES (
-    Engineering
-  ), (
-    Finance
-  ), (
-    Legal
-  ), (
-    Sales
-  );
+INSERT INTO departments (names)
+VALUES (Sales), (Engineering), (Finance), (Legal);
 
-INSERT INTO
-  role (
-    
-  )
-VALUE
+INSERT INTO roles (title, salary, department_id)
+VALUE ('Sales Lead', 100000, 1), ('Salesperson', 80000, 1), ('Lead Engineer', 150000, 2), ('Software Engineer', 120000, 2), ('Account Manager', 160000, 3), ('Accountant', 125000, 3), ('Legal Team Lead', 250000, 4), ('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUE (
+    'Robert', 'Bruce', 1, NULL
+  ), (
+    'Anne', 'Radcliffe', 1, 1
+  ), (
+    'Charles', 'Brown', 2, NULL
+  ), (
+    'Eliza', 'Parsons', 2, 2
+  ), (
+    'Susan', 'Hill', 3, NULL
+  ), (
+    'Sydney', 'Owenson', 3, 3
+  ), (
+    'Lisa', 'Crackanthorpe', 4, NULL
+  ), (
+    'Hubert ', 'Carleton', 4, 4
+  );
