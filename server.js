b5db62cd9c42e1d2fcf50ce511e7480c8daa8eca
Ron Dronet <ron@dronetdevdesign.com>
2022-08-14 12:57:33 -0500

    created routes for the employees

--- server.js
+++ server.js
@@ -20,9 +20,37 @@ const db = mysql.createConnection(
 );
 
 // return ALL data from employees table:
-db.query(`SELECT * FROM employees`, (err, rows) => {
-  console.log(rows);
+// db.query(`SELECT * FROM employees`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single employee:
+db.query(`SELECT * FROM employees WHERE ID = 1`, (err, row) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(row);
 });
+
+// DELETE a single employee:
+// db.query(`DELETE FROM employees WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// CREATE new employee:
+const sql = `INSERT INTO employees (id, first_name, last_name, role_id, manager_id) VALUES (?,?,?,?,?)`;
+const params = [1, 'Robert', 'Bruce', 1, null];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
+
 
 // test connection:
 app.get('/', (req, res) => {
