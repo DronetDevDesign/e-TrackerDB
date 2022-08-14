40d27c26c5bf984c1c6d49fabdddce65aae80111
Ron Dronet <ron@dronetdevdesign.com>
2022-08-14 12:39:44 -0500

    connected mysql database: employee_db to application using express

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require("mysql2");
 
 // Express middleware:
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// connect to MySQL database -> 'employee_db':
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username is:
+    user: 'root',
+    // MySQL password is:
+    password: 'Bu$$sawyer2022',
+    database: 'employee_db'
+  },
+  console.log('Connected to the employee_db database.')
+);
+
+// return ALL data from employees table:
+db.query(`SELECT * FROM employees`, (err, rows) => {
+  console.log(rows);
+});
 
 // test connection:
 app.get('/', (req, res) => {
